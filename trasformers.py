b65c1451ce1b59bff370964363358f70e296473a
Stanislav Osichniuk <stanhjrpower@gmail.com>
2021-09-23 11:51:39 +0300

    add any methods

--- trasformers.py
+++ trasformers.py
@@ -6,6 +6,13 @@ class Transformers:
         self.armor = armor
         self.age = age
 
+    def get_damaged(self, damage: int) -> float:
+        self.health -= damage * self.armor/100
+
+    def health_info(self):
+        return self.health
+
+
 
 class Autobot(Transformers):
     type_race = 'autobot'
@@ -24,3 +31,10 @@ megatron = Decepticon('Megatron', 'Gun', 195, 15, 953)
 print(optimus_prime.get_info())
 print()
 print(megatron.get_info())
+
+while megatron.health > 0:
+    damage_bot = int(input('Hit me!'))
+    megatron.get_damaged(damage_bot)
+    print(megatron.health_info())
+
+print('You Win!')
