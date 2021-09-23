6e11ae12c8fe495abda73908c2aedb89f2447949
Stanislav Osichniuk <stanhjrpower@gmail.com>
2021-09-23 11:38:42 +0300

    add class autobot and decepticon

--- trasformers.py
+++ trasformers.py
@@ -6,14 +6,21 @@ class Transformers:
         self.armor = armor
         self.age = age
 
+
+class Autobot(Transformers):
+    type_race = 'autobot'
+
     def get_info(self):
-        return f"This is {self.name} \nmode {self.modes} \nheath {self.health} \narmor {self.armor} \nage {self.age}"
+        return f"This is {self.name} \ntype: {self.type_race} \nmode: {self.modes} \nheath: {self.health} \narmor: {self.armor} \nage: {self.age}"
 
-class Autobots(Transformers):
 
-    def __init__(self):
-        type_race = 'autobot'
+class Decepticon(Autobot):
+    type_race = 'decepticon'
 
-optimus_prime = Transformers('Optimus_Prime', 'truck', 200, 20, 1152)
+
+optimus_prime = Autobot('Optimus_Prime', 'truck', 200, 20, 1152)
+megatron = Decepticon('Megatron', 'Gun', 195, 15, 953)
 
 print(optimus_prime.get_info())
+print()
+print(megatron.get_info())
