d2d0a9ccfb92d82b9db043c18094592424b14346
Stanislav Osichniuk <stanhjrpower@gmail.com>
2021-09-28 13:03:03 +0300

    remarks eliminated

--- trasformers.py
+++ trasformers.py
@@ -1,13 +1,13 @@
 import random
 
 
-class Transformers:
-    def __init__(self, name: str, modes: str, health: int, armor: int, age: int):
+class Transformer:
+    def __init__(self, name: str, modes: str, health: int, armor: int):
         self.name = name
         self.modes = modes
         self.health = health
         self.armor = armor
-        self.age = age
+
 
     def get_damaged(self, damage: int) -> int:
         self.health -= int(damage * self.armor / 50)
@@ -15,17 +15,23 @@ class Transformers:
     def health_info(self):
         return self.health
 
+    def health_print(self):
+        print('Health level = ', self.health)
 
-class Autobot(Transformers):
+
+class Autobot(Transformer):
     type_race = 'autobot'
     phrase_list = ['insidious decepticon!', 'I am the best!', 'on guard for good!', 'Ill kill you!',
                    'you can do better']
 
     def get_info(self):
-        return f"Im {self.name} \ntype: {self.type_race} \nmode: {self.modes} \nheath: {self.health} \narmor: {self.armor} \nage: {self.age}"
+        return f"Im {self.name} \ntype: {self.type_race} \nmode: {self.modes} \nheath: {self.health} \narmor: {self.armor}"
 
     def get_phrase(self):
         return random.choice(self.phrase_list)
+
+    def print_phrase(self):
+        print(random.choice(self.phrase_list))
 
 
 class Decepticon(Autobot):
@@ -33,10 +39,10 @@ class Decepticon(Autobot):
     phrase_list = ['haha, you are too weak', 'try again', 'oh shit!', 'kill me baby', 'Ill kill you!']
 
 
-optimus_prime = Autobot('Optimus_Prime', 'truck', 200, 20, 1152)
-megatron = Decepticon('Megatron', 'Gun', 195, 15, 953)
+optimus_prime = Autobot('Optimus_Prime', 'truck', 200, 20)
+megatron = Decepticon('Megatron', 'Gun', 195, 15)
 
-side = input('Enter side d/a (Decepticon/Autobot')
+side = input('Enter side d/a (Decepticon/Autobot)')
 
 while True:
     if side == 'a':
@@ -56,11 +62,11 @@ print()
 while bot.health > 0:
     damage_bot = int(input('Hit me!'))
     bot.get_damaged(damage_bot)
-    if bot.health < 0:
-        print("OVERKILL")
+    if bot.health < -20:
+        print("OVERKILL!")
         break
 
-    print('Health level = ', bot.health_info())
-    print(bot.get_phrase())
+    bot.health_print()
+    bot.print_phrase()
 
 print('\nCongrarulation, You Win!')
